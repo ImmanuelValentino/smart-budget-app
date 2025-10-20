b087837b354cdb10cb72c23f152bee9db127245a
immanuelm <imanuelv203@gmail.com>
2025-10-20 10:18:46 +0700

    Fixing CORS

--- server/index.js
+++ server/index.js
@@ -6,20 +6,25 @@ const connectDB = require('./config/db');
 const startServer = async () => {
     try {
         await connectDB();
-
         const app = express();
         const port = process.env.PORT || 5000;
 
-        app.use(cors());
+        // --- 👇👇 PERBARUI KONFIGURASI CORS DI SINI 👇👇 ---
+        const corsOptions = {
+            // Ganti dengan URL Vercel Anda yang sebenarnya
+            origin: 'https://smart-budget-app-taupe.vercel.app/',
+            optionsSuccessStatus: 200
+        };
+        app.use(cors(corsOptions));
+        // --- 👆👆 BATAS AKHIR PERUBAHAN 👆👆 ---
+
         app.use(express.json());
 
         app.use('/api/users', require('./routes/userRoutes'));
         app.use('/api/transactions', require('./routes/transactionRoutes'));
         app.use('/api/categories', require('./routes/categoryRoutes'));
-        app.use('/api/accounts', require('./routes/accountRoutes')); //
+        app.use('/api/accounts', require('./routes/accountRoutes'));
         app.use('/api/budgets', require('./routes/budgetRoutes'));
-
-
 
         app.get('/', (req, res) => {
             res.send('API is running successfully!');
